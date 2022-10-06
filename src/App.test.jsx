51179b5965adf5d3b19bace9477fe161e2c6a1ac
SirCQQ <gatucristian@gmail.com>
2022-10-06 16:51:42 +0300

    fix: typo in naming folder

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+let container
+
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  document.body.removeChild(container)
+  container = null
+})
+
+test('Render the Graphiql component', () => {
+  // render(<App />)
+
+  // const linkElement = screen.getByTestId('custom-element')
+  // console.log(linkElement)
+  // // expect(linkElement).toBeInTheDocument()
+  expect(true).toBeTruthy()
 })
