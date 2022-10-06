74b4c3ff0e11318a3f4b9647aed09cc02a2a0540
SirCQQ <gatucristian@gmail.com>
2022-10-06 21:35:34 +0300

    feat: add raw view of __explain object

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import { GraphiQL } from 'graphiql'
 import 'graphiql/graphiql.css'
 import { useExplainPlugin } from './graphiql-explain'
 import { fetcher } from './utils'
-
+import '@graphiql/react/dist/style.css'
 function App() {
   return (
-    <div style={{ height: '100vh' }}>
-      <p data-testid="custom-element">This is rendered</p>
+    <div style={{ height: '100vh', width: '1080px', overflow: 'scroll' }}>
       <GraphiQL fetcher={fetcher} plugins={[useExplainPlugin()]} />
     </div>
   )
--- src/graphiql-explain/Explain.jsx
+++ src/graphiql-explain/Explain.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useEditorContext, useResponseEditor } from '@graphiql/react'
+export function Content(props) {
+  const [__explain, setExplain] = useState()
 
-export function Content() {
-  return <div>This is the content </div>
+  const editorContext = useEditorContext({
+    nonNull: true,
+    caller: useResponseEditor()
+  })
+  useEffect(() => {
+    if (editorContext.tabs[editorContext.activeTabIndex].response) {
+      const response = JSON.parse(
+        editorContext.tabs[editorContext.activeTabIndex].response
+      )
+      if (response?.data?.__explain) {
+        setExplain([...response.data.__explain])
+        delete response.data.__explain
+        editorContext.tabs[editorContext.activeTabIndex].response =
+          JSON.stringify(response)
+      }
+    }
+  }, [editorContext])
+  return (
+    <div style={{ height: '100%' }}>
+      {__explain &&
+        __explain.length > 0 &&
+        __explain.map(e => {
+          return (
+            <div key={e.path} style={{ display: 'flex' }}>
+              <p style={{ margin: '5px 10px' }}>{e.path}</p>
+              <p style={{ margin: '5px 10px' }}>{e.start}</p>
+              <p style={{ margin: '5px 10px' }}>{e.stop}</p>
+              <p style={{ margin: '5px 10px' }}>{e.time}</p>
+            </div>
+          )
+        })}
+    </div>
+  )
 }
 export function Icon() {
   return <p>GE</p>
