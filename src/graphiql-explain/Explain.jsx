88e3463ee11b21b94137cff3d63bf11de4dcdee2
SirCQQ <gatucristian@gmail.com>
2022-10-12 08:41:48 +0300

    feat: add sort by table column, add filter by name with input

--- src/graphiql-explain/Explain.jsx
+++ src/graphiql-explain/Explain.jsx
@@ -1,21 +1,71 @@
 import React from 'react'
 import { useExplain } from './useExplain'
+import arrow from '../icons/arrow.svg'
 
 export function Content() {
-  const { explain } = useExplain()
+  const {
+    explain,
+    searchByPath,
+    changePathOrder,
+    changeTimeOrder,
+    timeOrder,
+    pathOrder
+  } = useExplain()
 
   return (
     <div style={{ height: '100%' }}>
+      <input type="text" onChange={searchByPath} placeholder="Search by path" />
       <table style={{ width: '100%' }}>
         <thead>
           <tr>
-            <th style={{ textAlign: 'left' }}>Path</th>
-            <th style={{ textAlign: 'left' }}>Time (ms) </th>
+            <th
+              style={{
+                textAlign: 'left',
+                display: 'flex',
+                alignItems: 'center'
+              }}
+              onClick={changePathOrder}
+            >
+              Path
+              {pathOrder !== 0 && (
+                <img
+                  src={arrow}
+                  style={{
+                    height: '100%',
+                    width: '20px',
+
+                    rotate: pathOrder > 0 ? '0deg' : '180deg'
+                  }}
+                  alt={`arrow-${pathOrder > 0 ? 'up' : 'down'}`}
+                />
+              )}
+            </th>
+            <th
+              style={{
+                textAlign: 'left',
+                display: 'flex',
+                alignItems: 'center'
+              }}
+              onClick={changeTimeOrder}
+            >
+              Time (ms)
+              {timeOrder !== 0 && (
+                <img
+                  src={arrow}
+                  style={{
+                    height: '100%',
+                    width: '20px',
+
+                    rotate: timeOrder > 0 ? '0deg' : '180deg'
+                  }}
+                  alt={`arrow-${timeOrder > 0 ? 'up' : 'down'}`}
+                />
+              )}
+            </th>
           </tr>
         </thead>
         <tbody>
-          {explain &&
-            explain.length > 0 &&
+          {explain && explain.length > 0 ? (
             explain.map(e => {
               return (
                 <tr key={e.path}>
@@ -23,7 +73,12 @@ export function Content() {
                   <td>{(e.time * 1e-6).toFixed(2)} </td>
                 </tr>
               )
-            })}
+            })
+          ) : (
+            <tr>
+              <td>No entry found</td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
--- src/graphiql-explain/useExplain.js
+++ src/graphiql-explain/useExplain.js
@@ -3,10 +3,17 @@ import { explainDataManager } from './ExplainDataManager'
 
 export const useExplain = () => {
   const [explain, setExplain] = useState(explainDataManager.getExplainData())
+  const [defaultExplain, setDefaultExplain] = useState(
+    explainDataManager.getExplainData()
+  )
+  const [pathOrder, setPathOrder] = useState(0)
+  const [timeOrder, setTimeOrder] = useState(0)
+
   useEffect(() => {
     const eventListener = explainDataManager.addEventListener(
       'updateExplainData',
       (e, value) => {
+        setDefaultExplain(_ => e.target.explain || [])
         setExplain(_ => e.target.explainData || [])
       }
     )
@@ -14,5 +21,69 @@ export const useExplain = () => {
       explainDataManager.removeEventListener('updateExplainData', eventListener)
     }
   }, [])
-  return { explain }
+
+  const searchByField = fieldName => {
+    return event => {
+      setExplain(_ => {
+        return defaultExplain.filter(element => {
+          return element[fieldName].includes(event.target.value)
+        })
+      })
+    }
+  }
+
+  const sort = (fieldName = 'path') => {
+    return order => {
+      let localOrder = order
+      if (!order || order === 0) {
+        return explain
+      } else if (order >= 1) {
+        localOrder = 1
+      } else {
+        localOrder = -1
+      }
+      setExplain(_ => {
+        return explain.sort((a, b) => {
+          return localOrder * `${a[fieldName]}`.localeCompare(`${b[fieldName]}`)
+        })
+      })
+    }
+  }
+
+  const sortPath = sort('path')
+  const sortTime = sort('time')
+
+  const changePathOrder = () => {
+    if (pathOrder !== 0) {
+      setPathOrder(prev => -prev)
+    } else {
+      setPathOrder(1)
+    }
+    setTimeOrder(0)
+  }
+
+  const changeTimeOrder = () => {
+    if (timeOrder !== 0) {
+      setTimeOrder(prev => -prev)
+    } else {
+      setTimeOrder(1)
+    }
+    setPathOrder(0)
+  }
+  useEffect(() => {
+    sortPath(pathOrder)
+  }, [pathOrder, sortPath])
+
+  useEffect(() => {
+    sortTime(timeOrder)
+  }, [timeOrder, sortTime])
+
+  return {
+    explain,
+    searchByPath: searchByField('path'),
+    changePathOrder,
+    changeTimeOrder,
+    timeOrder,
+    pathOrder
+  }
 }
