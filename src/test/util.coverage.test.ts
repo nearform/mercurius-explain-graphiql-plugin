e2f204f438550b751d0bb6c022ced5f627bce74b
Bruno Pascottini <bruno.pascottini@nearform.com>
2022-10-19 11:15:03 +0100

    refactor: tests with totals with readable numbers

--- src/test/util.coverage.test.ts
+++ src/test/util.coverage.test.ts
@@ -7,7 +7,7 @@ import {
   appendTotalsToExplainResponse
 } from '../graphiql-explain/utils'
 import { explainDataManager } from '../graphiql-explain/ExplainDataManager'
-import { data, gqlp } from './mockData'
+import { data, gqlp, simplifiedData } from './mockData'
 import { fetcherWrapper } from '../fetcherWrapper'
 
 let mockData
@@ -38,53 +38,54 @@ describe('Check the utils functions', () => {
   })
 
   it('appendTotalsToExplainResponse should add totals for each path of explain', () => {
-    const mockExplain = [...mockData.extensions.explain.profiler.data]
+    const mockSimplifiedData = JSON.parse(JSON.stringify(simplifiedData))
+    const mockExplain = [...mockSimplifiedData.extensions.explain.profiler.data]
     const response = appendTotalsToExplainResponse(mockExplain)
     expect(response).toEqual([
       {
+        begin: 1,
+        end: 2,
         path: 'users',
-        begin: 2180524057777125,
-        end: 2180524359067041,
-        time: 301289916,
-        totalBegin: 2180524057777125,
-        totalEnd: 2180524560907916,
-        totalTime: 503130791
+        time: 1,
+        totalBegin: 1,
+        totalEnd: 4,
+        totalTime: 3
       },
       {
+        begin: 2,
+        end: 4,
         path: 'users.0.addresses',
-        begin: 2180524359216958,
-        end: 2180524480481083,
-        time: 121264125,
-        totalBegin: 2180524359216958,
-        totalEnd: 2180524480481083,
-        totalTime: 121264125
+        time: 2,
+        totalBegin: 2,
+        totalEnd: 4,
+        totalTime: 2
       },
       {
+        begin: 3,
+        end: 4,
         path: 'users.1.addresses',
-        begin: 2180524359351708,
-        end: 2180524480591458,
-        time: 121239750,
-        totalBegin: 2180524359351708,
-        totalEnd: 2180524480591458,
-        totalTime: 121239750
+        time: 1,
+        totalBegin: 3,
+        totalEnd: 4,
+        totalTime: 1
       },
       {
+        begin: 1,
+        end: 3,
         path: 'users.0.status',
-        begin: 2180524359295541,
-        end: 2180524560765875,
-        time: 201470334,
-        totalBegin: 2180524359295541,
-        totalEnd: 2180524560765875,
-        totalTime: 201470334
+        time: 2,
+        totalBegin: 1,
+        totalEnd: 3,
+        totalTime: 2
       },
       {
+        begin: 1,
+        end: 2,
         path: 'users.1.status',
-        begin: 2180524359356041,
-        end: 2180524560907916,
-        time: 201551875,
-        totalBegin: 2180524359356041,
-        totalEnd: 2180524560907916,
-        totalTime: 201551875
+        time: 1,
+        totalBegin: 1,
+        totalEnd: 2,
+        totalTime: 1
       }
     ])
   })
