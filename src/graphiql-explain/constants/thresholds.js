32049b881cc165321426db8b6d4496e92618ba78
Bruno Pascottini <bruno.pascottini@nearform.com>
2022-10-31 14:55:07 +0000

    fix: threshold colours theme friendly (#57)
    * fix: threshold colors theme friendly
    
    * fix: standardize colours format

--- src/graphiql-explain/constants/thresholds.js
+++ src/graphiql-explain/constants/thresholds.js
@@ -1,27 +1,27 @@
 export const colors = {
-  yellowLight: '#ECE75F',
-  yellow: '#FFFF00',
-  orange: '#FFA500',
-  red: '#FF0000',
-  white: '#FFFFFF'
+  low: 'rgb(219 175 53)',
+  medium: 'rgb(255 165 0)',
+  high: 'rgb(255 78 78)',
+  veryHigh: 'rgb(255 0 0)',
+  default: 'inherit'
 }
 
 const thresholds = [
   {
     limit: 0.5,
-    color: colors.yellowLight
+    color: colors.low
   },
   {
     limit: 0.7,
-    color: colors.yellow
+    color: colors.medium
   },
   {
     limit: 0.9,
-    color: colors.orange
+    color: colors.high
   },
   {
     limit: 0.99,
-    color: colors.red
+    color: colors.veryHigh
   }
 ]
 
