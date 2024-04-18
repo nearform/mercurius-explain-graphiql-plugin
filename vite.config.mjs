9ccdd42e01c488ca6bfb6c7028a7800691ba3c04
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-04-18 21:14:15 +0200

    chore: remove rollup

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
@@ -24,9 +24,7 @@ export default defineConfig({
       formats: ['umd', 'cjs', 'es']
     },
     rollupOptions: {
-      external: [
-        'react'
-      ],
+      external: ['react'],
       output: {
         globals: {
           react: 'React'
